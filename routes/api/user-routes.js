ea5d12e8f87adc063f858e679730f0ad7e573d5a
reinholz36 <dreinholz28@gmail.com>
2022-02-07 13:04:21 -0500

    added password encryption

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -52,6 +52,7 @@ router.put('/:id', (req, res) => {
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
